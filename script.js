196d32d98ec5dde87b4b651f8748cbd041626cff
manojkumar1110 <139782634+manojkumar1110@users.noreply.github.com>
2023-08-22 15:51:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function changenumber1(){
+    var number1= Number(prompt("enter the number1:"));
+    var number2=Number(prompt("enter the number2:"));
+    alert(number1+number2);
+}
+function changenumber2(){
+    var number1= Number(prompt("enter the number1:"));
+    var number2=Number(prompt("enter the number2:"));
+    alert(number1-number2);
+}
+function changenumber3(){
+        var number1= Number(prompt("enter the number1:"));
+        var number2=Number(prompt("enter the number2:"));
+        alert(number1*number2);
+}
+function changenumber4(){
+    var number1= Number(prompt("enter the number1:"));
+    var number2=Number(prompt("enter the number2:"));
+    alert(number1/number2);
+}
+function changeimage(){
+    document.getElementById("img1").src="img4.jpg";
+}
+function changeimage5(){
+    document.getElementById("img1").src="img9.jpeg";
+}
